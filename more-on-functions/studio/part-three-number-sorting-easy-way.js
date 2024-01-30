7ea89d7f88867da3bbb3433a95f457fb8367f887
chrisbusche26 <chrisbusche26@gmail.com>
2024-01-29 21:04:42 -0600

    Class 6 studio

--- more-on-functions/studio/part-three-number-sorting-easy-way.js
+++ more-on-functions/studio/part-three-number-sorting-easy-way.js
@@ -4,5 +4,19 @@ let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Sort each array in ascending order.
+function ascend(arr) {
+    arr.sort(function (a, b) {
+    return a - b;
+    });
+    return arr;
+};
+console.log(ascend(nums2));
 
 //Sort each array in decending order.
+function descend(arr) {
+    arr.sort(function (a, b) {
+    return b - a;
+    });
+    return arr;
+};
+console.log(descend(nums2));
--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -20,10 +20,36 @@ function findMinValue(arr){
 
 //Your function here...
 
+function acsendingNum(arr) {
+  let sortedArray = [];
+  while (arr.length !== 0) {
+    let min = findMinValue(arr);
+    sortedArray.push(min);
+    arr.splice(arr.indexOf(min), 1);
+
+  }
+  return sortedArray;
+}
+
+
 /* BONUS MISSION: Refactor your sorting function to use recursion below:
- */
+*/
 
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
+function cursedSorting(arr) {
+  let sortedArray = [];
+  if (arr.length === 0) {
+    return sortedArray;
+  } else {
+    let min = findMinValue(arr);
+    sortedArray.push(min);
+    arr.splice(arr.indexOf(min), 1);
+    return findMinValue(arr);
+  }
+}
+
+console.log(cursedSorting(nums2));
