dabe086d8964d5d0a79fbadcfcc2e7079c6c2867
chrisbusche26 <chrisbusche26@gmail.com>
2024-02-26 18:38:51 -0600

    dom and events exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,28 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    //when take off button clicked, change text to Houston, we have liftoff!
+    button.addEventListener("click", function (event) {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+
+    missionAbort.addEventListener("mouseover", function (event) {
+        let button = event.target;
+        button.style.backgroundColor = "red";
+    });
     
+    missionAbort.addEventListener("mouseout", function (event) {
+        let button = event.target;
+        button.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener("click", function (event) {
+        let answer = window.confirm("Are you sure you want to abort the mission?");
+        if (answer === true) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        };
+    });
+
 }
 
 window.addEventListener("load", init);
